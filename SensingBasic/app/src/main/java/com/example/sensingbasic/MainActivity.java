ae63b70b661851c77b44fed1d706e2c99a293fe1
elijahchoi1030 <elijah1030@yonsei.ac.kr>
2021-07-12 20:48:07 +0900

    sensor done

--- SensingBasic/app/src/main/java/com/example/sensingbasic/MainActivity.java
+++ SensingBasic/app/src/main/java/com/example/sensingbasic/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.sensingbasic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.view.View;
 import android.widget.TextView;
 import android.content.Context;
 import android.hardware.Sensor;
@@ -12,42 +14,68 @@ import android.os.Bundle;
 
 import java.text.DecimalFormat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
-    SensorManager sensorManager;
-    Sensor lin_sen;
-    TextView t_acc_x;
-    TextView t_acc_y;
-    TextView t_acc_z;
+    private SensorManager sm;
+    private Sensor lin_sen;
+    private TextView t_acc_x;
+    private TextView t_acc_y;
+    private TextView t_acc_z;
+
+    private TextView tx;
+    private TextView ty;
+    private TextView tz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        lin_sen = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        lin_sen = sm.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
 
         t_acc_x = findViewById(R.id.acc_x);
         t_acc_y = findViewById(R.id.acc_y);
         t_acc_z = findViewById(R.id.acc_z);
+
+        tx = findViewById(R.id.textView2);
+        ty = findViewById(R.id.textView4);
+        tz = findViewById(R.id.textView5);
     }
 
-    public void onStart() {
-        super.onStart();
-
-
-        if(lin_sen != null) {
-            sensorManager.registerListener((SensorEventListener) this, lin_sen, SensorManager.SENSOR_DELAY_FASTEST);
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sm.registerListener(this, lin_sen, SensorManager.SENSOR_DELAY_UI);
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sm.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
 
     public void onSensorChanged(SensorEvent event) {
         if(event.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION){
 
-            t_acc_x.setText(new DecimalFormat("#.00").format(event.values[0]));
-            t_acc_y.setText(new DecimalFormat("#.00").format(event.values[1]));
-            t_acc_z.setText(new DecimalFormat("#.00").format(event.values[2]));
+            t_acc_x.setText(new DecimalFormat("#.0000").format(event.values[0]));
+            t_acc_y.setText(new DecimalFormat("#.0000").format(event.values[1]));
+            t_acc_z.setText(new DecimalFormat("#.0000").format(event.values[2]));
+
+            if(event.values[0] > 5)
+                tx.setTextColor(0xAAFF0000);
+            if(event.values[1] > 5)
+                ty.setTextColor(0xAAFF0000);
+            if(event.values[2] > 5)
+                tz.setTextColor(0xAAFF0000);
         }
+    }
+
+    public void resetColor(View view){
+        tx.setTextColor(Color.parseColor("#000000"));
+        ty.setTextColor(Color.parseColor("#000000"));
+        tz.setTextColor(Color.parseColor("#000000"));
     }
 }
